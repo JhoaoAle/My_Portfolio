496e3551dd5caf6294374350efcea9efd1077287
Jhoao <jhoao.martinez@utp.edu.co>
2026-01-30 13:51:52 -0500

    Version 0 of static portfolio website

--- mouse_tracking.js
+++ mouse_tracking.js
@@ -4,4 +4,12 @@ document.addEventListener('mousemove', function(event) {
     const x = event.clientX;
     const y = event.clientY;
     coordinatesDiv.textContent = `Mouse X: ${x}, Mouse Y: ${y}`;
+
+    const p = document.createElement('div');
+    p.className = 'particle';
+    p.style.left = x + 'px';
+    p.style.top = y + 'px';
+    document.body.appendChild(p);
+
+    setTimeout(() => p.remove(), 600);
 });
